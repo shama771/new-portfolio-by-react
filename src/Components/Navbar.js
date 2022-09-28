ca340edbb64f1850bc470259e13d9140367c2bde
shamanth <ushamanth771@gmail.com>
2022-09-28 09:23:19 +0530

    new-portfolio-by-react

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar(){
+    return(
+        <div className="navbar"> 
+         <h1 > Shamanth</h1>
+         <a href="#">Home</a>
+         <a href="#">Home</a>
+         <a href="#">Home</a>
+         <a href="#">Home</a>
+         <a href="#">Home</a>
+         </div>
+    )
+}
+export default Navbar;
